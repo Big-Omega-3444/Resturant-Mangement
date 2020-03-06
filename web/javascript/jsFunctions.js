7c2e1bf6c47979ef313ad99712d5277a5b8df539
Ayman Alqaq <aalqaq2@yahoo.com>
2020-03-06 17:06:00 -0600

    Start point (Front End)

---
+++ web/javascript/jsFunctions.js
@@ -0,0 +1,239 @@
+/** Help Functions **/
+
+function foodHelp(helpVal) { 
+  if (document.getElementById("foodHelp").innerHTML === "Help?") {
+    document.getElementById("foodHelp").innerHTML = "Your host is on the way!";
+	// Alert waiter
+  } else {
+    document.getElementById("foodHelp").innerHTML = "Help?";
+	// Cancel Request
+  }
+}
+
+function gameHelp(helpVal) { 
+  if (document.getElementById("gameHelp").innerHTML === "Help?") {
+    document.getElementById("gameHelp").innerHTML = "Your host is on the way!";
+	// Alert waiter
+  } else {
+    document.getElementById("gameHelp").innerHTML = "Help?";
+	// Cancel Request
+  }
+}
+
+function drinkHelp(helpVal) { 
+  if (document.getElementById("drinkHelp").innerHTML === "Help?") {
+    document.getElementById("drinkHelp").innerHTML = "Your host is on the way!";
+	// Alert waiter
+  } else {
+    document.getElementById("drinkHelp").innerHTML = "Help?";
+	// Cancel Request
+  }
+}
+
+/** End Help Functions **/
+
+/** Refill Functions **/
+
+function needRefill(helpVal) { 
+  if (document.getElementById("drinks1").innerHTML === "Refill") {
+    document.getElementById("drinks1").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks1").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+  
+function needRefill_1(helpVal) { 
+    if (document.getElementById("drinks2").innerHTML === "Refill") {
+    document.getElementById("drinks2").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks2").innerHTML = "Refill";
+	// Cancel Request
+  }
+ }
+ 
+function needRefill_2(helpVal) { 
+	if (document.getElementById("drinks3").innerHTML === "Refill") {
+    document.getElementById("drinks3").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks3").innerHTML = "Refill";
+	// Cancel Request
+  }
+ }
+ 
+function needRefill_3(helpVal) {  
+	if (document.getElementById("drinks4").innerHTML === "Refill") {
+    document.getElementById("drinks4").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks4").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+ 
+function needRefill_4(helpVal) { 
+	if (document.getElementById("drinks5").innerHTML === "Refill") {
+    document.getElementById("drinks5").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks5").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+ 
+function needRefill_5(helpVal) { 
+	if (document.getElementById("drinks6").innerHTML === "Refill") {
+    document.getElementById("drinks6").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks6").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+ 
+function needRefill_6(helpVal) { 
+	if (document.getElementById("drinks7").innerHTML === "Refill") {
+    document.getElementById("drinks7").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks7").innerHTML = "Refill";
+	// Cancel Request
+  }
+    }
+ 
+function needRefill_7(helpVal) { 
+	if (document.getElementById("drinks8").innerHTML === "Refill") {
+    document.getElementById("drinks8").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks8").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+ 
+function needRefill_8(helpVal) { 
+	if (document.getElementById("drinks9").innerHTML === "Refill") {
+    document.getElementById("drinks9").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks9").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+ 
+function needRefill_9(helpVal) { 
+	if (document.getElementById("drinks10").innerHTML === "Refill") {
+    document.getElementById("drinks10").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks10").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+ 
+function needRefill_10(helpVal) { 
+	if (document.getElementById("drinks11").innerHTML === "Refill") {
+    document.getElementById("drinks11").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks11").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+ 
+function needRefill_11(helpVal) { 
+	if (document.getElementById("drinks12").innerHTML === "Refill") {
+    document.getElementById("drinks12").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks12").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+ 
+function needRefill_12(helpVal) { 
+	if (document.getElementById("drinks13").innerHTML === "Refill") {
+    document.getElementById("drinks13").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks13").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+ 
+function needRefill_13(helpVal) { 
+	  if (document.getElementById("drinks14").innerHTML === "Refill") {
+    document.getElementById("drinks14").innerHTML = "Please wait...";
+	// Alert waiter
+  } else {
+    document.getElementById("drinks14").innerHTML = "Refill";
+	// Cancel Request
+  }
+}
+
+/** End Refills **/
+
+/** Begin Order Functions **/
+
+function orderFood1(helpVal) { 
+  if (document.getElementById("food1").innerHTML === "Order") {
+    document.getElementById("food1").innerHTML = "&#9989;";
+	// Alert waiter
+  } else {
+    document.getElementById("food1").innerHTML = "Order";
+	// Cancel Request
+  }
+}
+
+function orderFood2(helpVal) { 
+  if (document.getElementById("food2").innerHTML === "Order") {
+    document.getElementById("food2").innerHTML = "&#9989;";
+	// Alert waiter
+  } else {
+    document.getElementById("food2").innerHTML = "Order";
+	// Cancel Request
+  }
+}
+
+function orderFood3(helpVal) { 
+  if (document.getElementById("food3").innerHTML === "Order") {
+    document.getElementById("food3").innerHTML = "&#9989;";
+	// Alert waiter
+  } else {
+    document.getElementById("food3").innerHTML = "Order";
+	// Cancel Request
+  }
+}
+
+function orderFood4(helpVal) { 
+  if (document.getElementById("food4").innerHTML === "Order") {
+    document.getElementById("food4").innerHTML = "&#9989;";
+	// Alert waiter
+  } else {
+    document.getElementById("food4").innerHTML = "Order";
+	// Cancel Request
+  }
+}
+
+function orderFood5(helpVal) { 
+  if (document.getElementById("food5").innerHTML === "Order") {
+    document.getElementById("food5").innerHTML = "&#9989;";
+	// Alert waiter
+  } else {
+    document.getElementById("food5").innerHTML = "Order";
+	// Cancel Request
+  }
+}
+
+function orderFood6(helpVal) { 
+  if (document.getElementById("food6").innerHTML === "Order") {
+    document.getElementById("food6").innerHTML = "&#9989;";
+	// Alert waiter
+  } else {
+    document.getElementById("food6").innerHTML = "Order";
+	// Cancel Request
+  }
+}
