64ee0dfcf1cc6217d3bf4a93502159dbf8d92f51
E3245 <gibbly@zoho.com>
2020-04-07 19:08:59 -0500

    Added Edit Employees functionality for Manage Employees menu, making the Employees section fully editable.

--- app/static/javascript/management.js
+++ app/static/javascript/management.js
@@ -57,6 +57,52 @@ function SubmitForm()
     post.send(formData);
 }
 
+//On submit, PUT a form to the api
+//Not to be confused with SubmitForm(), which does a POST request
+function SubmitFormPUT()
+{
+    event.preventDefault();
+	
+    var put = new XMLHttpRequest();
+	
+	var url = "/api/employees/" + $('#editEmployee').find('#userID').val();
+
+    // POST to the API
+    put.open('PUT', url);
+	
+	// Handle errors	
+	//To Do: Alert user if errors occured, even OnLoad
+	put.error = function() 
+	{
+		alert("Request Failed!");
+	};	
+	
+	// Handle on load
+	put.onload = function() 
+	{
+		//Check for OK or CREATED status
+		if (put.status === 200 || put.status === 201 || put.status === 204)
+		{
+			//Close modal
+			$('#MGMT_EditEmployee').modal('hide');
+			$('#EmployeesTable tr td').remove();
+			requestData('/api/employees', '#EmployeesTable');
+		}
+		else
+		{
+			//TODO: Create alert in HTML instead of using this to specify error
+			var error = JSON.parse(put.responseText)
+			console.log(error.message)
+			
+			alert(`Error ${put.status}: ${error.message}`);
+		}
+	};
+
+    var formData = new FormData(document.getElementById("editEmployee"));
+	formData.delete('uid');		//This was just so we can retrieve it again in the future (which we did)
+    put.send(formData);
+}
+
 // Function that does a GET request on the specified API
 function requestData(url, selector)
 {
@@ -76,6 +122,40 @@ function requestData(url, selector)
 //			alert(`Done, got ${request.response.length} bytes`); // responseText is the server	
 			if (selector == '#EmployeesTable')
 				populateEmployeesTable(JSON.parse(request.responseText), selector)
+		}
+	};
+	
+	// Handle on errors	
+	request.error = function() 
+	{
+		alert("Request Failed!");
+	};
+
+	request.send();	
+}
+
+// Function that does a GET request on the specified API
+// This grabs a specific user using the same method as the deletion function
+function requestUser(object)
+{
+	// Create our XMLHttpRequest variable
+	var request = new XMLHttpRequest();
+	
+	// Create the url to retrieve user
+	var url = "/api/users/" + object.id;	
+	
+	request.open('GET', url);
+	
+	// Handle on load
+	request.onload = function() 
+	{
+		if (request.status === 200 || request.status === 201 || request.status === 204)
+		{	
+			autofillEditEmployeeForm(JSON.parse(request.responseText))
+		}
+		else
+		{
+			alert(`Error ${request.status}: ${request.statusText}`);
 		}
 	};
 	
@@ -146,21 +226,42 @@ function populateEmployeesTable(data, selector)
 		
 		//Create buttons for specific ID
 		var uid = data[i]._id.$oid;
-
+		
+		var editButton = $(`<button class="btn btn-secondary" id=${uid} data-toggle="modal" href="#MGMT_EditEmployee"/>`).click(function() {
+			requestUser(this);
+		}).html("EDIT");
 
 		var deleteButton = $(`<button class="btn btn-danger" id=${uid}/>`).click(function() {
 			deleteUser(this);
 		}).html("DEL");
 		
-		row.append($('<td/>').html(deleteButton));
+		row.append($('<td/>').html(editButton).append(deleteButton));
 		
-
-
 		$(selector).append(row);
 	}
 }
 
-// Event listener for specific modals
+// Simple function that autofills a selected form
+// data must be in JSON format
+function autofillEditEmployeeForm(data)
+{
+	$('#editEmployee').find('#userID').val(data._id.$oid);
+	$('#editEmployee').find('#firstnameTxtBox').val(data.firstname);
+	$('#editEmployee').find('#lastnameTxtBox').val(data.lastname);
+	$('#editEmployee').find('#address_streetTxtBox').val(data.address_street);
+	$('#editEmployee').find('#address_numberTxtBox').val(data.address_number);
+	$('#editEmployee').find('#address_cityTxtBox').val(data.address_city);
+	$('#editEmployee').find('#address_stateDropDown').val(data.address_state);
+	$('#editEmployee').find('#address_zipTxtBox').val(data.address_zip);
+	$('#editEmployee').find('#phone_numberTxtBox').val(data.phone_number);
+	$('#editEmployee').find('#emailTxtBox').val(data.email);	
+	$('#editEmployee').find('#assignmentDropDown').val(data.assignment);	
+	$('#editEmployee').find('#usernameTxtBox').val(data.username);	
+	$('#editEmployee').find('#pinTxtBox').val(data.pin);	
+}
+
+
+// Event listener for specific modals and other stuff
 // These listens for certain events on the management page and executes code when opened
 
 // Create the table once the modal is shown (after it pops up)
@@ -183,4 +284,9 @@ $('#MGMT_Employees').on('hide.bs.modal', function(event)
 
 $('#MGMT_Reports').on('show.bs.modal', function(){
    alert("Hello World!");
+});
+
+$('#MGMT_EditEmployee_btnSaveChanges').click( function()
+{
+	SubmitFormPUT();
 });
