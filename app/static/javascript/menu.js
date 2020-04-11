3302aa69bd5179f2ecf9dd19530f50de64de607b
CRaiden23 <14116906+CRaiden23@users.noreply.github.com>
2020-04-11 17:21:22 -0500

    Dynamic menus now display item cards correctly!

--- app/static/javascript/menu.js
+++ app/static/javascript/menu.js
@@ -6,10 +6,11 @@ function populateFoodTabs(data, selector)
 		var tab = $('<li class="nav-item"/>')
 
 		var tabName = data[i].name;
+		var refName = tabName.replace(" ", "_");
 		if (i == 0){
-			tab.append($(`<a href=${"#" + tabName} data-toggle="tab" class="nav-link active"/>`).html(tabName));
+			tab.append($(`<a href="${"#" + refName}" data-toggle="tab" class="nav-link active"/>`).html(tabName));
 		} else {
-			tab.append($(`<a href=${"#" + tabName} data-toggle="tab" class="nav-link"/>`).html(tabName));
+			tab.append($(`<a href="${"#" + refName}" data-toggle="tab" class="nav-link"/>`).html(tabName));
 		}
 
 		$(selector).append(tab);
@@ -18,17 +19,20 @@ function populateFoodTabs(data, selector)
 
 function populateFoodPane(data, selector)
 {
-	for(i = 0; i < data.length; i++) // iterate through menus again
+	for(i = 0; i < data.length; i++) // iterate through menus
 	{
 		var pane;
 		var tabName = data[i].name;
+		var refName = tabName.replace(" ", "_")
 
 		if (i == 0){ // first food tab gets the active pane
-			pane = $(`<div role="tabpanel" class="tab-pane active" id=${tabName}/>`);
+			pane = $(`<div role="tabpanel" class="tab-pane active" id="${refName}"/>`);
 		} else {
-			pane = $(`<div role="tabpanel" class="tab-pane" id=${tabName}/>`);
+			pane = $(`<div role="tabpanel" class="tab-pane" id="${refName}"/>`);
 		}
 		row = $('<div class="row"/>')
+		pane.append(row);
+
 		/*
 		const cardTemplate = `<div class="col-sm-4">
 									<div class="card-container manual-flip">
@@ -80,37 +84,86 @@ function populateFoodPane(data, selector)
 		pane.append(row);
 		 */
 
-		//populate food pane with items
-		for(j = 0; j < data[i].items.length; i++)
+		// append our pane and request our cards
+		$(selector).append(pane);
+		for(j=0; j < data[i].items.length; j++)
 		{
-			itemID = data[i].items[j];
-			url = "/api/menuitems/" + itemID;
+			var itemID = data[i].items[j].item.$oid;
+			var url = "/api/menuitems/" + itemID;
 
-			menuItem = requestData(url, "menuitem");
+			requestData(url, "#"+refName);
+		}
+	}
+}
 
-			var ingList = ""; // concatenate ingredients
-			for(k = 0; k < menuItem[i].ingredients.length; l++)
-			{
-				ingID = menuItem[i].ingredients[k];
-				ingurl = "/api/ingredients/" + ingID;
+function populateFoodCards(menuItem, selector)
+{
+	//populate food pane with items
+	var ingList = "Under Construction"; // concatenate ingredients
+	/*for (k = 0; k < data.ingredients.length; l++) {
+		var ingID = data.ingredients[k];
 
-				ingredient = requestData(url, "ingredient");
+		ingList = ingList + ingredient.name + ",";
+	}*/
 
-				ingList = ingList + ingredient.name + ",";
-			}
+	//Card Template
+	const cardTemplate = `<div class="col-sm-4">
+							<div class="card-container manual-flip">
+								<div class="card">
+									<div class="front">
+										<div class="content">
+											<div class="main">
+												<img class="card-img-top" src=${menuItem.image} style="width:50%">
+												<div class="card-body">
+													<h4 class="card-title">${menuItem.name}</h4>
+													<p class="card-text">${menuItem.description}</p>
+													<button type="button" id='${menuItem.name}' onclick="addOrder('${menuItem.name}')" class="btn btn-primary">Order</button>
+												</div>
+											</div>
+											<div class="footer">
+												<button class="btn btn-simple" onclick="healthFacts(this)">
+													<i class="fas fa-info-circle"></i> Health Facts
+												</button>
+											</div>
+										</div>
+									</div>
+									<div class="back">
+										<div class="header">
+											<h5 class="card-title">${menuItem.name}</h5>
+										</div>
+										<div class="content">
+											<div class="main">
+												<h4 class="text-center">Allergen</h4>
+												<p class="text-center">${ingList}</p>
 
-			// hopefully I can just use this template for the cards
-			const cardTemplate = `<div class="col-sm-4">
+												<div class="stats-container">
+													<div class="stats"><h4>Calories</h4><p>000</p></div>
+													<div class="stats"><h4>Fat</h4><p>000 grams</p></div>
+													<div class="stats"><h4>Protein</h4><p>000 grams</p></div>
+												</div>
+											</div>
+										</div>
+										<div class="footer">
+												<button type="button" class="btn btn-light" onclick="healthFacts(this)">
+													<i class="fas fa-backward"></i> Back
+												</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>`;
+	 /*
+	const cardTemplate = `<div class="col-sm-4">
 									<div class="card-container manual-flip">
 										<div class="card">
 											<div class="front">
 												<div class="content">
 													<div class="main">
-														<img class="card-img-top" src=${menuItem.image} style="width:100%">
+														<img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg" style="width:100%">
 														<div class="card-body">
-															<h4 class="card-title">${menuItem.name}</h4>
-															<p class="card-text">${menuItem.description}</p>
-															<button type="button" id=${menuItem.name} onclick="addOrder(${menuItem.name})" class="btn btn-primary">Order</button>
+															<h4 class="card-title">Title</h4>
+															<p class="card-text">Description</p>
+															<button type="button" id="Test" onclick="addOrder('Test')" class="btn btn-primary">Order</button>
 														</div>
 													</div>
 													<div class="footer">
@@ -122,13 +175,13 @@ function populateFoodPane(data, selector)
 											</div>
 											<div class="back">
 												<div class="header">
-													<h5 class="card-title">${menuItem.name}</h5>
+													<h5 class="card-title">Title</h5>
 												</div>
 												<div class="content">
 													<div class="main">
-														<h4 class="text-center">Allergen</h4>
-														<p class="text-center">${ingList}</p>
-
+														<h4 class="text-center"></h4>
+														<p class="text-center">Ingredient, ingredient, ingredient</p>
+			
 														<div class="stats-container">
 															<div class="stats"><h4>Calories</h4><p>000</p></div>
 															<div class="stats"><h4>Fat</h4><p>000 grams</p></div>
@@ -145,16 +198,10 @@ function populateFoodPane(data, selector)
 										</div>
 									</div>
 								</div>`;
+	  */
 
-			//append our template to the pane
-			row.append(cardTemplate);
-
-			pane.append(row);
-		}
-	}
-
-	// append our cards to the food panes
-	$(selector).append(pane);
+	//append our template to the pane
+	$(selector).find('div').append(cardTemplate);
 }
 
 function requestData(url, selector)
@@ -162,6 +209,8 @@ function requestData(url, selector)
 	// Create our XMLHttpRequest variable
 	var request = new XMLHttpRequest();
 	request.open('GET', url);
+
+
 
 	// Handle on load
 	request.onload = function()
@@ -177,8 +226,7 @@ function requestData(url, selector)
 			{
 				case'#foodTabs': populateFoodTabs(JSON.parse(request.responseText), selector); break;
 				case'#foodPane': populateFoodPane(JSON.parse(request.responseText), selector); break;
-				case'#drinkTabs': populateDrinksTabs(JSON.parse(request.responseText), selector); break;
-				default: return JSON.parse(request.responseText); break;
+				default: populateFoodCards(JSON.parse(request.responseText), selector); break; // if we're not doing the other things we must be doing cards
 			}
 		}
 	};
@@ -190,15 +238,6 @@ function requestData(url, selector)
 	};
 
 	request.send();
-}
-
-function updateMenus()
-{
-	$('#foodTabs li a').remove();
-	requestData('/api/menus', '#foodTabs');
-
-	$('#drinkTabs li a').remove();
-	requestData('/api/menus', '#drinkTabs');
 }
 
 // LISTENERS
