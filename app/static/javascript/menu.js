ddcb624d6ea5399d8b1e1242b09a7cf815052b28
CRaiden23 <14116906+CRaiden23@users.noreply.github.com>
2020-04-11 21:58:05 -0500

    Ingredients now display properly on menu cards

--- app/static/javascript/menu.js
+++ app/static/javascript/menu.js
@@ -3,6 +3,11 @@ function populateFoodTabs(data, selector)
 	//populate Food
 	for(i = 0; i < data.length; i++)
 	{
+		// check if the menu is active
+
+
+
+
 		var tab = $('<li class="nav-item"/>')
 
 		var tabName = data[i].name;
@@ -21,6 +26,10 @@ function populateFoodPane(data, selector)
 {
 	for(i = 0; i < data.length; i++) // iterate through menus
 	{
+		// check if the menu is active
+
+
+
 		var pane;
 		var tabName = data[i].name;
 		var refName = tabName.replace(" ", "_")
@@ -33,57 +42,6 @@ function populateFoodPane(data, selector)
 		row = $('<div class="row"/>')
 		pane.append(row);
 
-		/*
-		const cardTemplate = `<div class="col-sm-4">
-									<div class="card-container manual-flip">
-										<div class="card">
-											<div class="front">
-												<div class="content">
-													<div class="main">
-														<img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg" style="width:100%">
-														<div class="card-body">
-															<h4 class="card-title">Title</h4>
-															<p class="card-text">Description</p>
-															<button type="button" id="Test" onclick="addOrder('Test')" class="btn btn-primary">Order</button>
-														</div>
-													</div>
-													<div class="footer">
-														<button class="btn btn-simple" onclick="healthFacts(this)">
-															<i class="fas fa-info-circle"></i> Health Facts
-														</button>
-													</div>
-												</div>
-											</div>
-											<div class="back">
-												<div class="header">
-													<h5 class="card-title">Title</h5>
-												</div>
-												<div class="content">
-													<div class="main">
-														<h4 class="text-center"></h4>
-														<p class="text-center">Ingredient, ingredient, ingredient</p>
-			
-														<div class="stats-container">
-															<div class="stats"><h4>Calories</h4><p>000</p></div>
-															<div class="stats"><h4>Fat</h4><p>000 grams</p></div>
-															<div class="stats"><h4>Protein</h4><p>000 grams</p></div>
-														</div>
-													</div>
-												</div>
-												<div class="footer">
-														<button type="button" class="btn btn-light" onclick="healthFacts(this)">
-															<i class="fas fa-backward"></i> Back
-														</button>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>`;
-		//append our template to the pane
-		row.append(cardTemplate);
-		pane.append(row);
-		 */
-
 		// append our pane and request our cards
 		$(selector).append(pane);
 		for(j=0; j < data[i].items.length; j++)
@@ -91,7 +49,7 @@ function populateFoodPane(data, selector)
 			var itemID = data[i].items[j].item.$oid;
 			var url = "/api/menuitems/" + itemID;
 
-			requestData(url, "#"+refName);
+			requestData(url, "#"+refName, "menuitem");
 		}
 	}
 }
@@ -99,15 +57,9 @@ function populateFoodPane(data, selector)
 function populateFoodCards(menuItem, selector)
 {
 	//populate food pane with items
-	var ingList = "Ingredients Under Construction"; // concatenate ingredients
-	/*for (k = 0; k < data.ingredients.length; l++) {
-		var ingID = data.ingredients[k];
-
-		ingList = ingList + ingredient.name + ",";
-	}*/
 
 	//Card Template
-	const cardTemplate = `<div class="col-sm-4">
+	const cardTemplate = `<div id=${menuItem.name.replace(" ","_")} class="col-sm-4">
 							<div class="card-container manual-flip">
 								<div class="card">
 									<div class="front">
@@ -134,8 +86,7 @@ function populateFoodCards(menuItem, selector)
 										<div class="content">
 											<div class="main">
 												<h4 class="text-center">${menuItem.allergens}</h4>
-												<p class="text-center">${ingList}</p>
-
+												<p class="text-center">Ingredients</p>
 												<div class="stats-container">
 													<div class="stats"><h4></h4><p></p></div>
 													<div class="stats"><h4>Calories</h4><p>${menuItem.calories}</p></div>
@@ -152,8 +103,9 @@ function populateFoodCards(menuItem, selector)
 								</div>
 							</div>
 						</div>`;
-	 /*
-	const cardTemplate = `<div class="col-sm-4">
+
+	 /* Example card
+	const cardTemplate = `<div id="Title" class="col-sm-4">
 									<div class="card-container manual-flip">
 										<div class="card">
 											<div class="front">
@@ -201,10 +153,31 @@ function populateFoodCards(menuItem, selector)
 	  */
 
 	//append our template to the pane
-	$(selector).find('div').append(cardTemplate);
+	$(selector).find('div.row').append(cardTemplate);
+
+
+	for (k = 0; k < menuItem.ingredients.length; k++) {
+		var ingID = menuItem.ingredients[k].ingredient.$oid;
+		var url = "/api/ingredients/" + ingID;
+
+		requestData(url, selector, "ingredient");
+	}
+
+	//For later to fill in ingredients
+	//
 }
 
-function requestData(url, selector)
+function populateIngredients(ingredient, selector)
+{
+	var ingList = ingredient.name + ","; // concatenate ingredients
+	if($(selector).find('div div div.back div.content div p').innerHTML != undefined)
+		ingList = ingList + $(selector).find('div div div.back div.content div p').innerHTML;
+	alert(ingList);
+
+	$(selector).find('div div div.back div.content div.main p.text-center').replaceWith($(`<p class="text-center"/>`).html(ingList));
+}
+
+function requestData(url, selector, type)
 {
 	// Create our XMLHttpRequest variable
 	var request = new XMLHttpRequest();
@@ -226,7 +199,13 @@ function requestData(url, selector)
 			{
 				case'#foodTabs': populateFoodTabs(JSON.parse(request.responseText), selector); break;
 				case'#foodPane': populateFoodPane(JSON.parse(request.responseText), selector); break;
-				default: populateFoodCards(JSON.parse(request.responseText), selector); break; // if we're not doing the other things we must be doing cards
+				default:
+					switch(type)
+					{
+						case'menuitem': populateFoodCards(JSON.parse(request.responseText), selector); break; // if we're not doing the other things we must be doing cards
+						case'ingredient': populateIngredients(JSON.parse(request.responseText), selector); break;
+					} break;
+
 			}
 		}
 	};
@@ -245,8 +224,8 @@ function requestData(url, selector)
 // Create the table once the modal is shown (after it pops up)
 $('#Food').on('shown.bs.modal', function(event)
 {
-	requestData('/api/menus', '#foodTabs');
-	requestData('/api/menus', '#foodPane');
+	requestData('/api/menus', '#foodTabs', "UI");
+	requestData('/api/menus', '#foodPane', "UI");
 });
 
 // Remove the table's elements after the model is hidden
