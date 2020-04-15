5d9a8bedc2f16eb21c19bad44652381ca218a8e3
E3245 <gibbly@zoho.com>
2020-04-15 18:29:13 -0500

    Added working Order History to Reports modal

--- app/static/javascript/kitchen.js
+++ app/static/javascript/kitchen.js
@@ -47,16 +47,119 @@ function populateOrdersTable(orderData, menuItemsData)
 //			requestUser(this);
 			void(0);
 		}).html("CALL");
-
+	
 		row.append($('<td/>').html(readyButton).append(callButton));
-
-		$('#KTCH_OrderHistoryTable_Body').append(row);			
+		
+		$('#KTCH_OrderHistoryTable_Body').append(row);		
 	}	
 }
 
+function populateOrdersHistoryTable(orderData, menuItemsData)
+{
+	// Open a XHR request to retrieve employee database
+	// Create our XMLHttpRequest variable
+	var request = new XMLHttpRequest();
+	
+	// Create the url to retrieve user
+	var url = "/api/employees";	
+	
+	request.open('GET', url);
+	
+	// Handle on load
+	request.onload = function(data) 
+	{
+		if (request.status === 200 || request.status === 201 || request.status === 204)
+		{	
+			employeeData = JSON.parse(request.responseText);
+						
+			// Build the table
+			for(i = 0; i < orderData.length; i++) 
+			{
+				//Search for menu items
+				var arrMenuNames = [];
+		
+				for (j = 0; j < menuItemsData.length; j++)
+				{
+					for (k = 0; k < orderData[i].items.length; k++)
+					{
+						var str1 = (orderData[i].items[k].item.$oid).toString();
+						if (str1 === menuItemsData[j]._id.$oid)
+							arrMenuNames.push(menuItemsData[j].name);
+					}
+				}
+				
+				if (orderData[i].comped)
+					var row = $('<tr class="table-danger" id="tbl_orderID_${orderData[i]._id.$oid}"/>');
+				else
+					var row = $('<tr id="tbl_orderID_${orderData[i]._id.$oid}"/>');
+				
+				// Append first and last name into one variable
+//				var fullname = data[i].firstname + " " + data[i].lastname;
+				row.append($('<td/>').html("???"));
+				
+				var inject = $('<ul/>');
+				
+				//Now build and inject the bulleted list into the appended card
+				for (j = 0; j < arrMenuNames.length; j++)
+				{
+					inject.append($('<li/>').html(arrMenuNames[j]));			
+				}
+		
+				row.append($('<td/>').html(inject));
+				
+				var time = new Date(orderData[i].time_ordered);
+				var suffix = (time.getHours() >= 12) ? "AM" : "PM";
+				var minutes = (time.getMinutes() < 10) ? "0"+time.getMinutes() : time.getMinutes();
+				
+				row.append($('<td/>').html(`${time.getMonth() + 1}/${time.getDate()}/${time.getFullYear()} ${time.getHours()}:${minutes} ${suffix}`));
+		
+				if (orderData[i].total_cost != null)
+					row.append($('<td/>').html(`$${orderData[i].total_cost}`));
+				else
+					row.append($('<td/>').html('$0.00'));
+				
+				if (orderData[i].gratuity != null)				
+					row.append($('<td/>').html(`$${orderData[i].gratuity}`));
+				else
+					row.append($('<td/>').html('$0.00'));
+				
+				if (orderData[i].comped)
+				{
+					for (j = 0; j < employeeData.length; j++)
+					{
+						var str = (employeeData[j]._id.$oid).toString();
+						if (str === orderData[i].staff_comped._id.$oid)
+						{
+							row.append($('<td/>').html(`${employeeData[j].firstname} ${employeeData[j].lastname}`));
+							j = employeeData.length;					
+						}
+					}
+				}								
+				else
+					row.append($('<td/>').html("NO"));
+		
+		
+				$('#KTCH_OrderHistoryTable_Body').append(row);	
+			}	
+			return;
+		}
+		else
+		{
+			alert(`Error ${request.status}: ${request.statusText}`);
+		}
+	};
+	
+	// Handle on errors	
+	request.error = function() 
+	{
+		alert("Request Failed!");
+	};
+	
+	request.send();	
+}
 //Separate function because it's very messy
 // Does two XHRs to retrieve data from the database, then moves onto the BuildOrderCards
-function RetrieveOrders(BuildCards)
+function RetrieveOrders(BuildCards, Management)
 {
 	// Create our XMLHttpRequest variable
 	var request = new XMLHttpRequest();
@@ -98,11 +201,19 @@ function RetrieveOrders(BuildCards)
 				}
 				else
 				{
+					// Do a third XHR and retrieve employee table
+					if (Management === true)
+					{						
+						populateOrdersHistoryTable(data.target.extraInfo, JSON.parse(request2.responseText))
+						return;
+					}
+
 					// Check if we are building cards or a table
 					if (BuildCards === true)
 						BuildOrderCards(data.target.extraInfo, JSON.parse(request2.responseText))
 					else
-						populateOrdersTable(data.target.extraInfo, JSON.parse(request2.responseText))
+						populateOrdersTable(data.target.extraInfo, JSON.parse(request2.responseText), Management)
+
 				}
 			}
 			
@@ -206,7 +317,7 @@ function removeOrderCard(button)
 }
 
 // Outside so the script calls this function repeatedly every minute
-setInterval(RetrieveOrders(true), 10000);
+setInterval(RetrieveOrders(true, false), 10000);
 
 //
 // BEGIN Event Listeners
@@ -214,12 +325,12 @@ setInterval(RetrieveOrders(true), 10000);
 
 // Retrieve order cards on page load
 $( document ).ready(function() {
-    RetrieveOrders(true);
+    RetrieveOrders(true, false);
 });
 
 $('#KCHN_Orders').on('shown.bs.modal', function(event)
 {
-    RetrieveOrders(false);
+    RetrieveOrders(false, false);
 });
 
 $('#KCHN_Orders').on('hide.bs.modal', function(event)
