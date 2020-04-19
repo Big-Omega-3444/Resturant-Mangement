1db19d03127120c4afe1edd119661e9fc0f1866e
jl0844 <jl0844@unt.edu>
2020-04-19 10:41:39 -0500

    added employee_comped to coupon

--- app/models/CouponModel.py
+++ app/models/CouponModel.py
@@ -6,6 +6,7 @@ import json
 import logging
 from models.TemplateModel import TemplateResource, TemplateResourceList
 from models.MenuItemModel import MenuItemModel
+from models.MyBooleanField import MyBooleanField
 
 class ItemDiscount(EmbeddedDocument):
     item = ReferenceField('MenuItemModel', required=True)
@@ -31,6 +32,7 @@ class CouponModel(Document):
     specific_discounts = EmbeddedDocumentListField(ItemDiscount, default=[])
     percent_discount = FloatField(min_value=0, max_value=100)
     constant_discount = FloatField(min_value=0)
+    employee_comped = MyBooleanField(default=False)
 
     def clean(self):
         if self.specific_discounts is None and self.percent_discount == 0 and self.constant_discount == 0:
