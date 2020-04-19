66c6e9b0d301c0486b6545ec60a8f50fe325a682
Rony-Raju <Rony.Raju@my.unt.edu>
2020-04-19 05:30:12 -0500

    Function calls fixed

--- app/static/javascript/TableMap.js
+++ app/static/javascript/TableMap.js
@@ -1,13 +1,41 @@
-function allowDrop(ev) {
-  ev.preventDefault();
+// Retrieve order cards on page load
+$( document ).ready(function() {
+  var request = new XMLHttpRequest();
+  request.open('GET', "/api/tables");
+
+});
+
+function CreateTableButtons(data) {
+
+    for(i = 0; i < data.length; i++) // iterate through table list
+    {
+        console.log(data.length)
+    }
 }
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
+function GetTable() {
+	// Create our XMLHttpRequest variable
+	var request = new XMLHttpRequest();
+	request.open('GET', "/api/tables");
 
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+	// Handle on load
+	request.onload = function()
+	{
+		if (request.status != 200)
+		{
+			alert(`Error ${request.status}: ${request.statusText}`);
+		}
+		else
+		{
+			scanForTable(JSON.parse(request.responseText)); // Look through our tables
+		}
+	};
+
+	// Handle on errors
+	request.error = function()
+	{
+		alert("Request Failed!");
+	};
+
+	request.send();
 }
