5a8cbaaed236fa4a42d0c18641a74fbe6be8ee4e
jl0844 <jl0844@unt.edu>
2020-04-22 00:36:16 -0500

    Added default admin that get recreated everytime there is no manager account

--- app/models/EmployeeModel.py
+++ app/models/EmployeeModel.py
@@ -6,6 +6,7 @@ import json
 import logging
 
 from models.UserModel import UserModel, UserResource, UserResourceList
+from models.TemplateModel import TemplateResource, TemplateResourceList
 
 class EmployeeModel(UserModel):
     address_street = StringField(required=True)
@@ -18,12 +19,35 @@ class EmployeeModel(UserModel):
     phone_number = StringField(required=True)
 
 
-class EmployeeResource(UserResource):
+class EmployeeResource(TemplateResource):
     model = EmployeeModel
     pass
 
-class EmployeeResourceList(UserResourceList):
+default_admin_user = "admin"
+default_admin_pin = 1234
+
+class EmployeeResourceList(TemplateResourceList):
     model = EmployeeModel
+    def get(self):
+        # current_app.logger.debug("TYPE EMP: {}".format(type(self)))
+        # Add a default manager if there isn't one
+        ct = self.model.objects(assignment='manager').count()
+        if ct < 1:
+            current_app.logger.info("CREATED DEFAULT ADMIN")
+            new_admin = EmployeeModel()
+            new_admin.address_street = "X"
+            new_admin.address_city = "X"
+            new_admin.address_state = "X"
+            new_admin.address_zip = -1
+            new_admin.assignment = "manager"
+            new_admin.pin = default_admin_pin
+            new_admin.phone_number = "X"
+            new_admin.username = default_admin_user
+            new_admin.firstname = "ADMIN"
+            new_admin.email = "admin@admin.gov"
+            new_admin.save()
+        # Call the other get
+        return super().get()
     pass
 
 
